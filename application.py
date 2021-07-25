4faa19cfea4de05ee6dcf90b994e67679fd300d4
jonathan-sudo <johnlegrand972@gmail.com>
2021-07-25 21:24:57 +0200

    Fix the timed out error when connecting to RDS

--- application.py
+++ application.py
@@ -45,5 +45,4 @@ def index():
 
 
 if __name__ == '__main__':
-    print(application.config)
     application.run(host='0.0.0.0')
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 # edit the URI below to add your RDS password and your AWS URL
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ec2-user:26AwsDR5e3Y9hkLOgYrr@flask-aws-db.cifxtwfwpb84.us-east-2.rds.amazonaws.com:3306/flask-aws-db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:26AwsDR5e3Y9hkLOgYrr@flask-aws-db.cifxtwfwpb84.us-east-2.rds.amazonaws.com:3306/flask-aws-db'
 
 # Uncomment the line below if you want to work with a local DB
 # basedir = os.path.abspath(os.path.dirname(__file__))
