bdb0cc544ff4d26a339d996cd9d79305df5c44d0
jonathan-sudo <johnlegrand972@gmail.com>
2021-07-25 19:12:12 +0200

    Fixing the Track modification error, by deleting one application and moving the templates

--- application/__init__.py
+++ application/__init__.py
@@ -3,4 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
 application.config.from_object('config')
+# Elastic Beanstalk initalization
+application.debug = True
+# change this to your own value
+application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
+
 db = SQLAlchemy(application)
