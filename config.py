b197b46a780b40890db418a2ec1652d7d5a51a56
jonathan-sudo <johnlegrand972@gmail.com>
2021-08-04 23:34:00 +0200

    Changing the db information in config

--- config.py
+++ config.py
@@ -2,7 +2,12 @@ import os
 # edit the URI below to add your RDS password and your AWS URL
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:26AwsDR5e3Y9hkLOgYrr@flask-aws-db.cifxtwfwpb84.us-east-2.rds.amazonaws.com:3306/flask-aws-db'
+host = "mysql-test.cifxtwfwpb84.us-east-2.rds.amazonaws.com"
+port = 3306
+dbname = "mysql-test"
+user = "admin"
+password = "mysql-test"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + user + ":" + password + "@" + host + ":" + port + "/" + dbname
 
 # Uncomment the line below if you want to work with a local DB
 # basedir = os.path.abspath(os.path.dirname(__file__))
@@ -14,5 +19,5 @@ SQLALCHEMY_POOL_RECYCLE = 3600
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
 
-# Fiw for the track modification error
+# Fix for the track modification error
 SQLALCHEMY_TRACK_MODIFICATIONS = False
